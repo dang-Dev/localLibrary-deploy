ea4e42c4a6a736b2fd6b7ef55bdf256ce680590b
aljohnvillacruel141@gmail.com <aljohnvillacruel141@gmail.com>
2021-12-15 13:41:47 +0800

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nu_t53h(ozho^z8fj%m7znj%+is=)z!$=f3)im_=ucmi*vf^!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
