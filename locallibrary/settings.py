7f09f2ef2a2b8af58c13274edaddd7a3048a24df
aljohnvillacruel141@gmail.com <aljohnvillacruel141@gmail.com>
2021-12-15 13:51:03 +0800

    First version of application moved into github

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nu_t53h(ozho^z8fj%m7znj%+is=)z!$=f3)im_=ucmi*vf^!!'
+#SECRET_KEY = 'django-insecure-nu_t53h(ozho^z8fj%m7znj%+is=)z!$=f3)im_=ucmi*vf^!!'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nu_t53h(ozho^z8fj%m7znj%+is=)z!$=f3)im_=ucmi*vf^!!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+#DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['*']
 
 
